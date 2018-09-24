25bc444498a22a04b227ab4e9a19815acc8c79be
tsudodog <malamute29@gmail.com>
2018-09-24 16:20:58 -0500

    upgraded my gitstatus scanner

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+/*jshint esversion: 6*/
 var fs = require('fs');
 var exec = require('child_process').exec;
 var _ = require('lodash');
@@ -8,64 +9,76 @@ var clc = require('cli-color');
 
 
 
-program.version('0.0.1')
-    .option('-h, -help', 'Load Help')
+program
+    .version('0.0.1')
+    .option('-v, --verbose', 'Verbose Output')
     .parse(process.argv);
 
 
 
 
 exec("git status", (err, stdout, stderr) => {
-    // console.log(__dirname,'\n\n');
-    // console.log('currentWorkingDirectory:\t', process.cwd());
-    // console.log(stdout);
- 
-    let modifiedFiles = _.split((_.split(stdout, 'Untracked files:')[0]), "Changes to be committed:")[1];
-    // console.log(modifiedFiles);
-    let unrevisedFiles = filterFiles(modifiedFiles);
-    console.log(unrevisedFiles)
+    if(program.verbose){
+        console.log('GITHUB STANDARD OUTPUT:\n');
+        console.log(clc.green(stdout));
+        console.log('END OF GITHUB STANDARD OUTPUT')
+    }
+   
 
-    unrevisedFiles.forEach((s)=>{
+    let lineSplit      =  _.split(_.split(stdout, "Changes to be committed:\n")[1], '\n');
+    let stopAdding = false;
+    let nArrayOfFiles = lineSplit.filter( (s) => {
+        if(_.startsWith(s, "\t") && !stopAdding){
+            console.log('Scanning:', clc.yellowBright(s.replace("\t","")));
+            
+            return s;
+        }
+        if(_.endsWith(s, ":")){
+            stopAdding = true;
+        }
+    } );
+
+    let newFileList = filterFiles(nArrayOfFiles);
+
+    newFileList.forEach((s)=>{
         openFile(s);
     });
 
-
-    // console.log(unrevisedFiles);
 });
 
 
 let openFile = (fileLocation) => {
     let absolutePath = process.cwd() + '/'+fileLocation;
-    console.log('ABSOLUTEPATH:\t', absolutePath);
     fs.readFile(absolutePath, 'utf8', function(err, data){
         if(err){
             return console.log('openFile readFile threw exception \n', err);
         }
-
         examineText(data, fileLocation);
-        // console.log(data);
-    })
+       
+    });
 };
 
 
 let filterFiles = (input) => {
-    let arrayOfLines = _.split(input, '\n');
-     let filteredArrayOfLines = arrayOfLines.filter(line => line.startsWith("\t"));
-
-     filteredArrayOfLines.forEach((s,n,a) => {
-         let insideVal = s.replace('\t','' );
-         insideVal = _.split(insideVal, ':')[1]
-         insideVal = _.trimStart(insideVal);
-         a[n] = insideVal
-     });    
-    return filteredArrayOfLines;
-}
+    return input.map( (s)=>{
+        let insideVal = s.replace('\t','' );
+        insideVal = _.split(insideVal, ':')[1];
+        insideVal = _.trimStart(insideVal);
+        return insideVal;
+     });
+};
 
 let examineText = (fileText, fileLocation) => {
     let lines = _.split(fileText, '\n');
     for(let c = 0 ; c<lines.length; c++){
         if((lines[c].indexOf('System.out') > -1)){
-            console.log(clc.red('FILE:\t', fileLocation, ' contains a System.out on line ', c+1))  ;
+            console.log(clc.red('FILE:\t', fileLocation, ' contains a System.out on line ', c+1));
+        }
+        if((lines[c].indexOf('console.log')> -1)){
+            console.log(clc.red('FILE:\t', fileLocation, ' contains a console.log on line ', c+1));
+        }
+        if((lines[c].indexOf('debugger')> -1)){
+            console.log(clc.redBright('FILE:\t', fileLocation, ' contains a debugger on line ', c+1));
         }
     }    
-}
+};
